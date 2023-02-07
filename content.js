b3e04c17eb695ba454a58781dca44114a0f3cdac
vinuthabr <42146405+vinuthabr@users.noreply.github.com>
2023-02-07 14:48:33 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -31,6 +31,51 @@ function buildSearchKey(searchQuery) {
     if(searchQuery.indexOf("gifts") !=-1 || searchQuery.indexOf("gift") !=-1 ) {
         return "gift";
     }
+    if(searchQuery.indexOf("bralette") !=-1 || searchQuery.indexOf("bralettes") !=-1 ) {
+        return "bralette";
+    }
+    if(searchQuery.indexOf("shapewear") !=-1 || searchQuery.indexOf("shape") !=-1 ) {
+        return "shapewear";
+    }
+    if(searchQuery.indexOf("teddy") !=-1 || searchQuery.indexOf("teddies") !=-1 ) {
+        return "teddy";
+    }
+    if(searchQuery.indexOf("robes") !=-1 || searchQuery.indexOf("robe") !=-1 ) {
+        return "robes";
+    }
+    if(searchQuery.indexOf("pajama") !=-1 || searchQuery.indexOf("pajamas") !=-1 ) {
+        return "pajama";
+    }
+    if(searchQuery.indexOf("swim") !=-1 || searchQuery.indexOf("swimsuits") !=-1 ) {
+        return "swim";
+    }
+    if(searchQuery.indexOf("leggings") !=-1 || searchQuery.indexOf("legging") !=-1 ) {
+        return "leggings";
+    }
+    if(searchQuery.indexOf("jumpsuit") !=-1 || searchQuery.indexOf("jumpsuits") !=-1 ) {
+        return "jumpsuit";
+    }
+    if(searchQuery.indexOf("short") !=-1 || searchQuery.indexOf("shorts") !=-1 ) {
+        return "shorts";
+    }
+    if(searchQuery.indexOf("fragrance") !=-1 || searchQuery.indexOf("perfume") !=-1 ) {
+        return "fragrance";
+    }
+    if(searchQuery.indexOf("mist") !=-1 || searchQuery.indexOf("mists") !=-1 ) {
+        return "mist";
+    }
+    if(searchQuery.indexOf("lotion") !=-1 || searchQuery.indexOf("lotions") !=-1 ) {
+        return "lotion";
+    }
+    if(searchQuery.indexOf("bags") !=-1 || searchQuery.indexOf("bag") !=-1 ) {
+        return "bags";
+    }
+    if(searchQuery.indexOf("backpack") !=-1 || searchQuery.indexOf("backpacks") !=-1 ) {
+        return "backpack";
+    }
+    if(searchQuery.indexOf("deodorant") !=-1 || searchQuery.indexOf("deo") !=-1 ) {
+        return "deodorant";
+    }
     return null;  
 }
 
